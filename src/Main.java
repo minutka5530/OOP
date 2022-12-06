5eeec9386386e6c4ad7300c3e180770a810345b6
reichdarian <minutka5530@gmail.com>
2022-12-06 20:01:30 +0700

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person maksim = new Person(1988,"Мaксим","Минск", "бренд-менеджером");
+
+        Person anya = new Person(1993, "Аня", "Москва", "методистом образовательных программ");
+
+        Person katya = new Person(1992, "Катя", "Калининград", "продакт-менеджером");
+
+        Person artem = new Person(-15 ,"", "Москва","директором по развитию бизнеса");
+
+        System.out.println(maksim);
+        System.out.println(anya);
+        System.out.println(katya);
+        System.out.println(artem);
+
+
+        System.out.println();
+
+
+        Car lada = new Car("Lada","Granta",1.5, "Желтый", 2015, "Россия");
+        Car audi = new Car("Audi","A8 50 L TDI quattro",3.0, "Черный",
+                2020, "Германия");
+        Car bmw = new Car("BMW","Z8",3.0, "Черный", 2021, "Германия");
+        Car kia = new Car("Kia","Sportage",2.4, "красный", 2015,
+                "Южной Корее");
+        Car hyundai = new Car("Hyundai"," Avante",1.6, "Оранжевый", 2016,
+                "Южной Корее");
+        System.out.println(lada);
+        System.out.println(audi);
+        System.out.println(bmw);
+        System.out.println(kia);
+        System.out.println(hyundai);
+
+
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,37 @@
+public class Person {
+    int yearOfBirth;
+    String name;
+    String town;
+    String jobTitle;
+
+
+
+    Person(int yearOfBirth, String name, String town, String jobTitle) {
+        if(name==null || name.isEmpty()){
+            name = "НЕ указал";
+        }
+        this.name = name;
+        if(town==null || town.isEmpty()){
+            town = "НЕ указал";
+        }
+
+        this.town = town;
+        if (yearOfBirth < 0){
+            this.yearOfBirth = 0;
+        } else {
+            this.yearOfBirth = Math.abs(yearOfBirth);
+        }
+        if(jobTitle==null || jobTitle.isEmpty()){
+            jobTitle = "НЕ указал";
+        }
+
+        this.jobTitle = jobTitle;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут " + name + " Я из города " + town + " Я родился в " + yearOfBirth + " году. " +
+                "Я работаю на должности " + jobTitle + " . Будем знакомы!";
+    }
+}
